375d5fea1d2311c00a102f061752c499d1fa1c0a
gdiasdlima <gdiasdlima@yahoo.com.br>
2021-07-20 14:35:17 -0300

    feat : add Model UserProfile

---
+++ src/data/models/userProfile.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm'
+import { UserModel } from './user';
+
+@Entity('user_profile')
+export class UserProfileModel {
+  @PrimaryGeneratedColumn('increment')
+  id: number
+
+  @OneToOne(() => UserModel)
+  @JoinColumn({ name: 'ID_USER' })
+  user: UserModel;
+
+  @OneToOne(() => Profile)
+  @JoinColumn({ name: 'ID_PROFILE' })
+  profile: Profile;
+
+}
